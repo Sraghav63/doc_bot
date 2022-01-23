a00147f98c4b4339723dc35c01e7257a5a9b9387
Sraghav63 <97779701+Sraghav63@users.noreply.github.com>
2022-01-23 10:01:25 +0530

    last change to ui on question page

--- lib/ai.dart
+++ lib/ai.dart
@@ -12,22 +12,26 @@ class question extends StatefulWidget {
 }
 
 class _questionState extends State<question> {
-
   TextEditingController textController1 = TextEditingController();
   TextEditingController textController2 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-
-        padding: const EdgeInsets.only(top: 70, left: 15, right: 30),
+        padding: const EdgeInsets.only(top: 50, left: 15, right: 30),
         child: Column(
           children: [
             ElevatedButton(
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: Icon(Icons.home),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Icon(Icons.arrow_back_ios),
+              ),
+            ),
+            SizedBox(
+              height: 20,
             ),
             TextFormField(
               keyboardType: TextInputType.multiline,
@@ -35,42 +39,44 @@ class _questionState extends State<question> {
               decoration: InputDecoration(
                 hintText: "Enter your question here",
                 labelText: "Enter your question here",
-
-
               ),
               controller: textController1,
             ),
-
-            ElevatedButton(onPressed: () async {
-              final String aidocURL = "aidoc.loca.lt";
-              print("trying to get answer to question: ");
-              print(textController1.text.trim());
-              var queryParameters = {
-                "question": textController1.text.trim()
-              };
-              var uri = Uri.https(aidocURL,"/medicaladvice",queryParameters);
-              Response res = await get(uri, headers: {
-                HttpHeaders.contentTypeHeader: "application/json"
-              });
-              if (res.statusCode == 200) {
-                String answer = (res.body);
-                print("answer is: ");
-                print(answer);
-                textController2.text = answer;
-              }else{
-                //TODO: report error
-                print("error getting response from aidoc");
-              }
-
-            }, child: Text("Enter")),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  final String aidocURL = "aidoc.loca.lt";
+                  print("trying to get answer to question: ");
+                  print(textController1.text.trim());
+                  var queryParameters = {
+                    "question": textController1.text.trim()
+                  };
+                  var uri = Uri.https(aidocURL, "/medicaladvice", queryParameters);
+                  Response res = await get(uri, headers: {
+                    HttpHeaders.contentTypeHeader: "application/json"
+                  });
+                  if (res.statusCode == 200) {
+                    String answer = (res.body);
+                    print("answer is: ");
+                    print(answer);
+                    textController2.text = answer;
+                  } else {
+                    //TODO: report error
+                    print("error getting response from aidoc");
+                  }
+                },
+                child: Text("Enter")),
+            SizedBox(
+              height: 20,
+            ),
             TextFormField(
               keyboardType: TextInputType.multiline,
               maxLines: null,
               controller: textController2,
               readOnly: true,
             ),
-
-
           ],
         ),
       ),
