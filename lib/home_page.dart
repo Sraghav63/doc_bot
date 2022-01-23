ea0c33013ac8aa97487cc9caebb92ef2834ae63b
Sraghav63 <97779701+Sraghav63@users.noreply.github.com>
2022-01-23 09:32:33 +0530

    Ui fix

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,161 +1,161 @@
-  import 'package:doc_bot/ai.dart';
-  import 'package:flutter/cupertino.dart';
-  import 'package:flutter/material.dart';
-  import 'colors.dart' as color;
-  import 'package:doc_bot/covid_Page.dart';
-  import 'package:doc_bot/medicalnews_Page.dart';
-  import 'package:get/get.dart';
+import 'package:doc_bot/ai.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'colors.dart' as color;
+import 'package:doc_bot/covid_Page.dart';
+import 'package:doc_bot/medicalnews_Page.dart';
+import 'package:get/get.dart';
 
-  class HomePage extends StatefulWidget {
-    const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-    @override
-    _HomePageState createState() => _HomePageState();
-  }
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-  class _HomePageState extends State<HomePage> {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: color.AppColor.homePageBackground,
-        body: Container(
-          padding: const EdgeInsets.only(top: 70, left: 12, right: 20),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Text(
-                    "How are you today",
-                    style: TextStyle(
-                        fontSize: 30,
-                        color: color.AppColor.homePageTitle,
-                        fontWeight: FontWeight.w700),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                children: [
-                  Text(
-                    "Covid Updates and News",
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: color.AppColor.homePageSubtitle,
-                        fontWeight: FontWeight.w700),
-                  ),
-                  Expanded(child: Container()),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => covidPage()));
-                    },
-                    child: Text('View'),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Container(
-                    width: 370,
-                    height: 150,
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.lightBlueAccent,
-                            offset: const Offset(3.0, 3.0),
-                            blurRadius: 10,
-                            spreadRadius: 2.0,
-                          )
-                        ],
-                        gradient: LinearGradient(colors: [
-                          color.AppColor.gradientFirst,
-                          color.AppColor.gradientSecond
-                        ]),
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(10),
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(10),
-                            topRight: Radius.circular(80))),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => question()));
-                        },
-                        style: ButtonStyle(
-                            shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(10),
-                                      bottomLeft: Radius.circular(10),
-                                      bottomRight: Radius.circular(10),
-                                      topRight: Radius.circular(80)),
-                                ))),
-                        child: Row(children: [
-                          Text(
-                            'Talk to our AI',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize: 20,
-                            ),
-                          )
-                        ])),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Row(
-                children: [
-                  Container(
-                    width: 370,
-                    height: 100,
-                    decoration: BoxDecoration(boxShadow: [
-                      BoxShadow(
-                        color: Colors.lightBlueAccent,
-                        offset: const Offset(3.0, 3.0),
-                        blurRadius: 10,
-                        spreadRadius: 2.0,
-                      )
-                    ]),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          shape:
-                          MaterialStateProperty.all<RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                  borderRadius:
-                                  BorderRadius.circular(10.0)))),
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: color.AppColor.homePageBackground,
+      body: Container(
+        padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "How are you today",
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: color.AppColor.homePageTitle,
+                      fontWeight: FontWeight.w700),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Covid Updates and News",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: color.AppColor.homePageSubtitle,
+                      fontWeight: FontWeight.w700),
+                ),
+                Expanded(child: Container()),
+                SizedBox(
+                  width: 5,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => covidPage()));
+                  },
+                  child: Text('View'),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width - 50,
+                  height: 150,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.lightBlueAccent,
+                          offset: const Offset(3.0, 3.0),
+                          blurRadius: 10,
+                          spreadRadius: 2.0,
+                        )
+                      ],
+                      gradient: LinearGradient(colors: [
+                        color.AppColor.gradientFirst,
+                        color.AppColor.gradientSecond
+                      ]),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                          topRight: Radius.circular(80))),
+                  child: ElevatedButton(
                       onPressed: () {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => medicalnewsPage()));
+                                builder: (context) => question()));
                       },
-                      child: Text(
-                        'Medical News!',
-                        style: TextStyle(fontSize: 20),
-                      ),
+                      style: ButtonStyle(
+                          shape:
+                          MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    bottomLeft: Radius.circular(10),
+                                    bottomRight: Radius.circular(10),
+                                    topRight: Radius.circular(80)),
+                              ))),
+                      child: Row(children: [
+                        Text(
+                          'Talk to our AI',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        )
+                      ])),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width - 50,
+                  height: 100,
+                  decoration: BoxDecoration(boxShadow: [
+                    BoxShadow(
+                      color: Colors.lightBlueAccent,
+                      offset: const Offset(3.0, 3.0),
+                      blurRadius: 10,
+                      spreadRadius: 2.0,
+                    )
+                  ]),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        shape:
+                        MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius:
+                                BorderRadius.circular(10.0)))),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => medicalnewsPage()));
+                    },
+                    child: Text(
+                      'Medical News!',
+                      style: TextStyle(fontSize: 20),
                     ),
-                  )
-                ],
-              ),
-            ],
-          ),
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
-      );
-    }
+      ),
+    );
   }
+}
